a6b9d6ea1fcc3e87557a48223b962c1ca7763b5b
Aditya Pooruli <aspoorul@ucsc.edu>
2022-07-11 11:52:26 -0700

    start

---
+++ track.py
@@ -0,0 +1,17 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+import numpy as np
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+ 
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
