f08bc766829c69f554e17e5acec1876bc1914cbc
Aditya Pooruli <aspoorul@ucsc.edu>
2022-07-28 11:50:42 -0700

    finished

--- mouse.py
+++ mouse.py
@@ -36,18 +36,38 @@ while True:
     if fingers[1] == 1 and fingers[2] == 0:
 
     # Step Five get and convert coordinates
-    x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
-    y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+        x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+        y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
 
     # Step Six value smoothing
-    clocX = plocX + (x3 - plocX) / smoothening
-    clocY = plocY + (y3 - plocY) / smoothening
+        clocX = plocX + (x3 - plocX) / smoothening
+        clocY = plocY + (y3 - plocY) / smoothening
 
     # Step Seven will be to move the mouse
+        autopy.mouse.move(wScr - clocX, clocY)
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        plocX, plocY = clocX, clocY
+
     # Step Eight if index and thumb finger are between a certain distance then click
+    if fingers[1] == 1 and fingers[2] == 1:
+
     # Step Nine will be to find the distance
+        length, img, lineInfo = detector.findDistance(8, 12, img)
+        # print(length)
+
     # Step Ten click the mouse if within the value
+    if length < 40:
+        cv2.circle(img, (lineInfo[4], lineInfo[5]),
+        15, (0, 255, 0), cv2.FILLED)
+        autopy.mouse.click()
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     # Step Eleven is to Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+
